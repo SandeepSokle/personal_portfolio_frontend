9e81a42ecabdd57bf8ffd47fbb409f952871fd2c
Sandeep Sokle <sandeepsokle12@gmail.com>
2022-05-28 00:52:23 +0530

    impememnt file store and get url

--- src/AdminPanelComponent/AdminProjects.js
+++ src/AdminPanelComponent/AdminProjects.js
@@ -9,6 +9,14 @@ import { getDataActionCreater } from "../Redux/getDataActionCreater";
 import { useDispatch, useSelector } from "react-redux";
 import { handleSave, handleUpdate } from "../HandleFunctions/handleFunctions";
 import { GeneralInputField } from "../GeneralComponents/GeneralInputField";
+import storage from "../firebase/firebase_config";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import uuid from "react-uuid";
 
 export const AdminProjects = (props) => {
   const { selectedTab } = props;
@@ -233,6 +241,80 @@ export const AdminProjects = (props) => {
                 sx={{
                   width: "48%",
                   m: 1,
+                }}
+                onChange={async (e) => {
+                  console.log("target", e.target.files);
+                  console.log("value", e.target.value);
+                  const storageRef = ref(
+                    storage,
+                    "images/" + uuid() + "_" + e.target.files[0].name
+                  );
+                  // const { url } = storageRef;
+
+                  // console.log("url::",url, storageRef);
+                  // const file = await uploadBytes(storageRef, e.target.files[0]);
+                  // .then((snapshot) => {
+                  // console.log("Uploaded a blob or file!", file);
+                  // });
+
+                  // const storage = getStorage();
+
+                  // Create the file metadata
+                  /** @type {any} */
+                  const metadata = {
+                    contentType: "image/jpeg",
+                  };
+
+                  // Upload file and metadata to the object 'images/mountains.jpg'
+                  const uploadTask = uploadBytesResumable(
+                    storageRef,
+                    e.target.files[0]
+                  );
+
+                  // Listen for state changes, errors, and completion of the upload.
+                  uploadTask.on(
+                    "state_changed",
+                    (snapshot) => {
+                      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                      const progress =
+                        (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                      console.log("Upload is " + progress + "% done");
+                      switch (snapshot.state) {
+                        case "paused":
+                          console.log("Upload is paused");
+                          break;
+                        case "running":
+                          console.log("Upload is running");
+                          break;
+                      }
+                    },
+                    (error) => {
+                      // A full list of error codes is available at
+                      // https://firebase.google.com/docs/storage/web/handle-errors
+                      switch (error.code) {
+                        case "storage/unauthorized":
+                          // User doesn't have permission to access the object
+                          break;
+                        case "storage/canceled":
+                          // User canceled the upload
+                          break;
+
+                        // ...
+
+                        case "storage/unknown":
+                          // Unknown error occurred, inspect error.serverResponse
+                          break;
+                      }
+                    },
+                    () => {
+                      // Upload completed successfully, now we can get the download URL
+                      getDownloadURL(uploadTask.snapshot.ref).then(
+                        (downloadURL) => {
+                          console.log("File available at", downloadURL);
+                        }
+                      );
+                    }
+                  );
                 }}
                 // value={selectedItem.name ? `${selectedItem.img}` : ""}
               />
---
+++ src/firebase/firebase_config.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getStorage, ref } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAHRk7a2frlaZqQ5bOlSIgxQ1yqiwMi5ow",
+  authDomain: "portfolio-d0b3d.firebaseapp.com",
+  projectId: "portfolio-d0b3d",
+  storageBucket: "portfolio-d0b3d.appspot.com",
+  messagingSenderId: "372520123661",
+  appId: "1:372520123661:web:254c47d3e5ed7c63fc8444",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+// const db = getFirestore(firebaseApp);
+const storage = getStorage();
+// const storageRef = ref(storage, "files");
+
+// module.exports = {
+//   db,
+//   storage,
+//   storageRef,
+// };
+
+export default storage;
