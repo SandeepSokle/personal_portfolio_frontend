6cca2b2dabe37c1b4bc5dbe4deac96ea75439198
Sandeep Sokle <sandeepsokle12@gmail.com>
2022-05-29 00:21:35 +0530

    up

--- src/firebase/firebase_config.js
+++ src/firebase/firebase_config.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getStorage,getFirestore } from "firebase/storage";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,9 +16,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
-const db = getFirestore(firebaseApp);
+// const db = getFirestore(firebaseApp);
 const storage = getStorage();
-console.log({ db });
+console.log({ firebaseApp });
 // const storageRef = ref(storage, "files");
 
 // module.exports = {
