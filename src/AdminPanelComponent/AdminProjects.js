f28074709046d81e7034f9b5ca3a9e2c5e358057
Sandeep Sokle <sandeepsokle12@gmail.com>
2022-05-29 00:10:35 +0530

    implement file uplaod done

--- src/AdminPanelComponent/AdminProjects.js
+++ src/AdminPanelComponent/AdminProjects.js
@@ -7,7 +7,11 @@ import { AdminResumeEducationData } from "../AdminPanelComponentHelper/AdminResu
 import EditIcon from "@mui/icons-material/Edit";
 import { getDataActionCreater } from "../Redux/getDataActionCreater";
 import { useDispatch, useSelector } from "react-redux";
-import { handleSave, handleUpdate } from "../HandleFunctions/handleFunctions";
+import {
+  fileUpload,
+  handleSave,
+  handleUpdate,
+} from "../HandleFunctions/handleFunctions";
 import { GeneralInputField } from "../GeneralComponents/GeneralInputField";
 import storage from "../firebase/firebase_config";
 import {
@@ -220,7 +224,7 @@ export const AdminProjects = (props) => {
                   style={{ marginRight: "12px" }}
                   target="_blank"
                   rel="noreferrer"
-                  href="https://primeprogrammingworld.blogspot.com/2021/06/leetcode-max-area-of-island-solution.html"
+                  href={`${selectedItem.file}`}
                 >
                   Project Image
                 </a>
@@ -244,77 +248,16 @@ export const AdminProjects = (props) => {
                 }}
                 onChange={async (e) => {
                   console.log("target", e.target.files);
-                  console.log("value", e.target.value);
-                  const storageRef = ref(
-                    storage,
-                    "images/" + uuid() + "_" + e.target.files[0].name
-                  );
-                  // const { url } = storageRef;
-
-                  // console.log("url::",url, storageRef);
-                  // const file = await uploadBytes(storageRef, e.target.files[0]);
-                  // .then((snapshot) => {
-                  // console.log("Uploaded a blob or file!", file);
-                  // });
-
-                  // const storage = getStorage();
-
-                  // Create the file metadata
-                  /** @type {any} */
-                  const metadata = {
-                    contentType: "image/jpeg",
-                  };
-
-                  // Upload file and metadata to the object 'images/mountains.jpg'
-                  const uploadTask = uploadBytesResumable(
-                    storageRef,
-                    e.target.files[0]
-                  );
-
-                  // Listen for state changes, errors, and completion of the upload.
-                  uploadTask.on(
-                    "state_changed",
-                    (snapshot) => {
-                      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-                      const progress =
-                        (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                      console.log("Upload is " + progress + "% done");
-                      switch (snapshot.state) {
-                        case "paused":
-                          console.log("Upload is paused");
-                          break;
-                        case "running":
-                          console.log("Upload is running");
-                          break;
-                      }
-                    },
-                    (error) => {
-                      // A full list of error codes is available at
-                      // https://firebase.google.com/docs/storage/web/handle-errors
-                      switch (error.code) {
-                        case "storage/unauthorized":
-                          // User doesn't have permission to access the object
-                          break;
-                        case "storage/canceled":
-                          // User canceled the upload
-                          break;
-
-                        // ...
-
-                        case "storage/unknown":
-                          // Unknown error occurred, inspect error.serverResponse
-                          break;
-                      }
-                    },
-                    () => {
-                      // Upload completed successfully, now we can get the download URL
-                      getDownloadURL(uploadTask.snapshot.ref).then(
-                        (downloadURL) => {
-                          console.log("File available at", downloadURL);
-                        }
-                      );
-                    }
-                  );
+                  let fileUrl = await fileUpload({
+                    file: e.target.files[0],
+                    dispatch,
+                    storeValue: "file",
+                    data,
+                    setData,
+                  });
+                  console.log("fileUrl :: before");
+                  console.log(fileUrl);
+                  console.log("fileUrl :: after");
                 }}
                 // value={selectedItem.name ? `${selectedItem.img}` : ""}
               />
--- src/HandleFunctions/handleFunctions.js
+++ src/HandleFunctions/handleFunctions.js
@@ -1,17 +1,26 @@
 import axios from "axios";
+import storage from "../firebase/firebase_config";
 import { getDataActionCreater } from "../Redux/getDataActionCreater";
-
+const uuid = require("react-uuid");
+const {
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} = require("firebase/storage");
 export const handleSave = async (props) => {
   const { selectedTab, selectedVal, data, dispatch } = props;
   //   const dispatch = useDispatch();
   console.log("Save Hit!!");
   try {
-    const response = await axios.post("https://dynamic-portfolio-api.herokuapp.com/portfolio/save", {
-      data: data,
-      id: "1234587678",
-      module: selectedTab.toLowerCase(),
-      type: selectedVal.toLowerCase(),
-    });
+    const response = await axios.post(
+      "https://dynamic-portfolio-api.herokuapp.com/portfolio/save",
+      {
+        data: data,
+        id: "1234587678",
+        module: selectedTab.toLowerCase(),
+        type: selectedVal.toLowerCase(),
+      }
+    );
     console.log(response.data);
 
     dispatch(getDataActionCreater());
@@ -25,7 +34,9 @@ export const handleDelete = async (props) => {
   //   const dispatch = useDispatch();
   console.log("Delete Hit!!", id);
   try {
-    const response = await axios.delete(`https://dynamic-portfolio-api.herokuapp.com/portfolio/delete/${id}`);
+    const response = await axios.delete(
+      `https://dynamic-portfolio-api.herokuapp.com/portfolio/delete/${id}`
+    );
     console.log(response.data);
 
     dispatch(getDataActionCreater());
@@ -39,10 +50,75 @@ export const handleUpdate = async (props) => {
   //   const dispatch = useDispatch();
   console.log("Update Hit!!", id);
   try {
-    const response = await axios.put(`https://dynamic-portfolio-api.herokuapp.com/portfolio/update/${id}`, data);
+    const response = await axios.put(
+      `https://dynamic-portfolio-api.herokuapp.com/portfolio/update/${id}`,
+      data
+    );
     console.log(response.data);
 
     dispatch(getDataActionCreater());
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const fileUpload = async (props) => {
+  const { file, dispatch, storeValue, setData, data } = props;
+  //   const dispatch = useDispatch()
+  try {
+    const storageRef = ref(storage, "images/" + uuid() + "_" + name);
+    let fileUrl = "";
+
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+        }
+      },
+      (error) => {
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+          case "storage/unauthorized":
+            // User doesn't have permission to access the object
+            break;
+          case "storage/canceled":
+            // User canceled the upload
+            break;
+
+          // ...
+
+          case "storage/unknown":
+            // Unknown error occurred, inspect error.serverResponse
+            break;
+        }
+      },
+      () => {
+        // Upload completed successfully, now we can get the download URL
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log("File available at", downloadURL);
+          let newData = data;
+          newData[`${storeValue}`] = downloadURL;
+          setData(newData);
+          fileUrl = downloadURL;
+        });
+      }
+    );
+
+    //updateData
+    return fileUrl;
   } catch (err) {
     console.log(err);
   }
